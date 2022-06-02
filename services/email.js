37d47243c83f45ac8ea5257b9d3b199ffe4fb895
Kunal <kunal.rohitas29@gmail.com>
2022-06-02 11:12:43 +0530

    Resolve security issues

--- services/email.js
+++ services/email.js
@@ -1,12 +1,10 @@
 const mailgun = require("mailgun-js");
 
-const DOMAIN = 'sandbox16cc6fe5b24842c1b7ff1dc25782a2b4.mailgun.org';
-
-const mg = mailgun({apiKey: "2f3b5516c515861116a9c5d794565cf1-27a562f9-5f08d709", domain: DOMAIN});
+const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
 
 const data = {
-	from: 'kunal.rohitas29@gmail.com',
-	to: 'kunal.rohitas29@gmail.com',
+	from: process.env.MAILGUN_SENDER,
+	to: process.env.MAILGUN_RECIEVER,
 	subject: 'Your Website Just went down',
 	text: 'Testing some Mailgun awesomness!'
 };
