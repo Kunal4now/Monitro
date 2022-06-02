315bb4711ffa09c5f5add7ec8f0ab21516df60fd
Kunal <kunal.rohitas29@gmail.com>
2022-06-02 23:43:08 +0530

    Add auth route

--- routes/monitor.js
+++ routes/monitor.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 const mailWrapper = require('../services/email')
+const User = require('../models/User')
+const fetchuser = require('../middleware/fetchUser')
 
 const website = process.env.SITE
 
-router.get('/monitor', async (req, res) => {
+router.get('/monitor', fetchuser, async (req, res) => {
     axios.interceptors.request.use((config) => {
         config.metadata = {startTime: new Date()}
         return config
@@ -20,26 +22,48 @@ router.get('/monitor', async (req, res) => {
         return Promise.reject(err)
     })  
 
-    let result = {}
-    axios.get(website).then((response) => {
-        result = {
-            time: new Date(),
-            sucess: true,
-            responseDuration: response.duration
+    const sites = await User.findOne({_id: req.user.id}).select('websites')
+    let site = sites.websites.map((doc) => {return doc.url})
+
+    monitor = async (website) => {
+        try {
+            const response = await axios.get(website).then((response) => {
+                return response
+            })
+            return obj = {
+                url: website,
+                time: new Date(),
+                sucess: true,
+                responseDuration: response.duration
+            }
+        } catch(error) {
+            return obj = {
+                url: website,
+                time: new Date(),
+                sucess: false
+            }
         }
-        return res.status(200).json(result)
-    }).catch((error) => {
-        result = {
-            time: new Date(),
-            error: error,
-            sucess: false
+    }
+
+    let promises = []
+    for (let i = 0; i < site.length; i++) {
+        promises.push(monitor(site[i]))
+    }
+
+    const result = await Promise.all(promises)
+
+    result.forEach((element) => {
+        if (!element.sucess) {
+            // User.find
         }
-        mailWrapper.data.text = `Please check your website ${website} has gone down`
-        // mailWrapper.mg.messages().send(data, function (error, body) {
-            //     console.log(body);
-        // });
-        return res.status(404).json(result)
-        })
     })
+
+        //     mailWrapper.data.text = `Please check your website ${website} has gone down`
+    //     // mailWrapper.mg.messages().send(data, function (error, body) {
+    //         //     console.log(body);
+    //     // });
+    
+    res.status(200).json(result)
+})
 
 module.exports = router
