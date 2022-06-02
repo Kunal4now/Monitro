b002e7cef7d45fd6cac5f6701d5c14dc69b7dc8b
Kunal <kunal.rohitas29@gmail.com>
2022-06-02 10:58:30 +0530

    Add email service

--- routes/monitor.js
+++ routes/monitor.js
@@ -1,24 +1,45 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
+const mailWrapper = require('../services/email')
 
 const website = process.env.SITE
 
 router.get('/monitor', async (req, res) => {
+    axios.interceptors.request.use((config) => {
+        config.metadata = {startTime: new Date()}
+        return config
+    }, (err) => {
+        return Promise.reject(err)
+    })
+    axios.interceptors.response.use((response) => {
+        response.config.metadata.endTime = new Date()
+        response.duration = response.config.metadata.endTime - response.config.metadata.startTime
+        return response
+    }, (err) => {
+        return Promise.reject(err)
+    })  
+
+    let result = {}
     axios.get(website).then((response) => {
-        err = {
+        result = {
             time: new Date(),
-            sucess: true
+            sucess: true,
+            responseDuration: response.duration
         }
-        return res.status(200).json(err)
+        return res.status(200).json(result)
     }).catch((error) => {
-        err = {
+        result = {
             time: new Date(),
             error: error,
             sucess: false
         }
-        return res.status(404).json(err)
+        mailWrapper.data.text = `Please check your website ${website} has gone down`
+        // mailWrapper.mg.messages().send(data, function (error, body) {
+            //     console.log(body);
+        // });
+        return res.status(404).json(result)
+        })
     })
-})
 
 module.exports = router
---
+++ services/email.js
@@ -0,0 +1,19 @@
+const mailgun = require("mailgun-js");
+
+const DOMAIN = 'sandbox16cc6fe5b24842c1b7ff1dc25782a2b4.mailgun.org';
+
+const mg = mailgun({apiKey: "2f3b5516c515861116a9c5d794565cf1-27a562f9-5f08d709", domain: DOMAIN});
+
+const data = {
+	from: 'kunal.rohitas29@gmail.com',
+	to: 'kunal.rohitas29@gmail.com',
+	subject: 'Your Website Just went down',
+	text: 'Testing some Mailgun awesomness!'
+};
+
+let mailWrapper = {
+    data: data,
+    mg: mg
+}
+
+module.exports = mailWrapper
