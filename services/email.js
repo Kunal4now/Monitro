535f491c0922c7ba6f3bd1594f46c7f3d362359e
Kunal <kunal.rohitas29@gmail.com>
2022-06-05 11:36:22 +0530

    Fix email service

--- services/email.js
+++ services/email.js
@@ -1,17 +1,6 @@
 const mailgun = require("mailgun-js");
 
-const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
 
-const data = {
-	from: process.env.MAILGUN_SENDER,
-	to: process.env.MAILGUN_RECIEVER,
-	subject: 'Your Website Just went down',
-	text: 'Testing some Mailgun awesomness!'
-};
-
-let mailWrapper = {
-    data: data,
-    mg: mg
+exports.mailWrapper = (reciever) => {
+	const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
 }
-
-module.exports = mailWrapper
