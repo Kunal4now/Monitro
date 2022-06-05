b9523630cc44efb6ded57e53248ee4bafa2f4e85
Kunal <kunal.rohitas29@gmail.com>
2022-06-05 11:36:43 +0530

    Add controllers

--- services/email.js
+++ services/email.js
@@ -2,5 +2,5 @@ const mailgun = require("mailgun-js");
 
 
 exports.mailWrapper = (reciever) => {
-	const mg = mailgun({process.env.MAILGUN_API_KEY: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_process.env.MAILGUN_DOMAIN});
+	const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
 }
