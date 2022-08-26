badfc7e0701aa22a5639ca0aab1ef435c36e496f
Kunal <kunal.rohitas29@gmail.com>
2022-08-26 21:43:05 +0530

    Add health and 404 routes

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/monitor', require('./routes/monitor'))
 app.use('/api/website', require('./routes/website'))
 
+app.get('/api/health', (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'Active'
+        })
+    } catch(err) {
+        res.status(500).json({
+            message: err
+        })
+    }
+})
+
+app.get('*', (req, res) => {
+    res.status(404).json({
+        message: 'Page not found'
+    })
+})
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
